bde93be74cf081f34510c8f5aa3d60c44fe3b563
sashasul2003@gmail.com <sashasul2003@gmail.com>
2020-01-07 06:38:51 +0300

    added builders for both keyboards

--- bot.py
+++ bot.py
@@ -1,90 +1,49 @@
-import requests
+from requests_proxy_bot_handler import *
+from telegram_api_for_python import *
 from time import sleep
 
-class ReplyKeyboardMarkup:
-    keyboard: list
-    resize_keyboard=True
-    one_time_keyboard=True
-    seletcive=False
-
-class KeyboardButton:
-    text: str
-    request_contact=False
-    request_location=False
-
-
-    
-
-    
-class BotHandler:
-
-    def __init__(self, token):
-        self.token = token
-        self.api_url = "https://api.telegram.org/bot{}/".format(token)
-
-    def get_updates(self, offset=None, timeout=30):
-        method = 'getUpdates'
-        params = {'timeout': timeout, 'offset': offset}
-        resp = requests.get(self.api_url + method, params)
-        result_json = resp.json()['result']
-        return result_json
-
-    def send_message(self, chat_id, text):
-        #params = {'chat_id': chat_id, 'text': text, "reply_markup":'{"inline_keyboard": [[{"text": "Последние операции","callback_data": "\/last"},{"text": "Баланс","callback_data": "\/balance"}]]}'}
-        #params = {"chat_id": chat_id, "text": text, "reply_markup": {"keyboard": [[{"text": "Start tour"}]]} }
-        #params = {'chat_id': chat_id, 'text': text, 'reply_markup': greet_kb }
-        #params = {'chat_id': chat_id, 'text': text}
-        #params = {'chat_id': chat_id, 'text': text, 'disable_notification': False, 'reply_markup': str(markup) }
-        #params = {'chat_id': chat_id, 'text': text, 'disable_notification': False, 'reply_markup': '{"keyboard": [["Start new tour"], ["Done"]], "resize_keyboard": true, "one_time_keyboard": true, "selective": false}'} #робит блэт
-        #params = {'chat_id': chat_id, 'text': text, 'reply_markup': '{"keyboard": [[{"text":"Start new tour"}]], "resize_keyboard": true, "one_time_keyboard": true, "selective": false}'}
-        #params = {'chat_id': chat_id, 'text': text, 'reply_markup': None}
-        method = 'sendMessage'
-        print(params)
-        resp = requests.post(self.api_url + method, params)
-        print(resp)
-        return resp
-        
-    def get_last_update(self):
-        get_result = self.get_updates()
-
-        if len(get_result) > 0:
-            last_update = get_result[-1]
-        else:
-            last_update = None
-
-        return last_update
-
-    def KeyboardBuilder(self, texts, request_contact=False,request_location=False): #example a = KeyboardBuilder([['HI','Wassup?']['BYE']])
-        for i in texts:
-            for j in i:
-                
-        
 token1 = "1012565455:AAGctwGzz0LRlucqZiiEIvchtLhJjd1Fqdw"
-greet_bot = BotHandler(token1)  
+eos_bot = BotHandler(token1) 
 
 
+        
+key1 = KeyboardBuilder([['HI','Wassup?'],['BYE']])
+key1 = key1.build()
+markup1 = ReplyKeyboardMarkupBuilder(key1)
+markup1 = markup1.build()
+
+but1 = InlineButtonBuilder('кнопка 1', callback_data = '/1')
+but1 = but1.build()
+but2 = InlineButtonBuilder('trashbox', url = 'http://trashbox.ru')
+but2 = but2.build()
+markup2 = InlineMarkupBuilder([[but1],[but2]])
+markup2 = markup2.build()
 
 def main():  
     new_offset = None
     print('Start')
     while True:        
-        greet_bot.get_updates(new_offset)
+        eos_bot.get_updates(new_offset)
 
-        last_update = greet_bot.get_last_update()
+        last_update = eos_bot.get_last_update()
         if last_update is None:
             continue
-        
         last_update_id = last_update['update_id']
-        last_chat_text = last_update['message']['text']
-        last_chat_id = last_update['message']['chat']['id']
-        last_chat_name = last_update['message']['chat']['first_name']
-
-        
+        if 'message' in last_update.keys():
+            last_chat_text = last_update['message']['text']
+            last_chat_id = last_update['message']['chat']['id']
+            last_chat_name = last_update['message']['chat']['first_name']
+        elif 'callback_query' in last_update.keys():
+            last_chat_text = last_update['callback_query']['data']
+            last_chat_id = last_update['callback_query']['message']['chat']['id']
+            last_chat_name = last_update['callback_query']['message']['chat']['first_name']
+            
         if last_chat_text.lower()=='/start':
-            greet_bot.send_message(last_chat_id, '''Приветствую, {}.
-Я - бот-помощник компании EOS Tour. Помогу Вам забронировать тур не сделав ни единого звонка!
-Сейчас я еще в разработке, но совсем скоро я смогу многое!
-                                   '''.format(last_chat_name))
+            eos_bot.send_message(last_chat_id, 'Приветствую, {}.'.format(last_chat_name), markup = ReplyKeyboardRemove)
+        elif last_chat_text.lower()=='/new':
+            eos_bot.send_message(last_chat_id, 'New, {}.'.format(last_chat_name), markup = markup2)
+        elif last_chat_text.lower()=='/kb':
+            eos_bot.send_message(last_chat_id, 'New, {}.'.format(last_chat_name), markup = markup1)
         new_offset = last_update_id + 1
 
         
