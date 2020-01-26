cc39471b7af8bc32a69f4518a220981038f3e789
sashasul2003@gmail.com <sashasul2003@gmail.com>
2020-01-26 12:27:02 +0300

    Restart now depends on os name

--- restart.py
+++ restart.py
@@ -5,9 +5,18 @@ Any cleanup action (like saving data) must be done before calling this function.
 """
 import logging
 from os import execl
+from os import name
 import sys
-def program():
-    logging.info("Restart!")
-    #exec(open('/home/pi/bot/bot_main.py').read())
-    python = sys.executable
-    execl(python, python, *sys.argv)
+from time import sleep
+
+
+def program(secs_of_sleep):
+    logging.info(f"Restart in {secs_of_sleep} seconds!")
+    sleep(secs_of_sleep)
+    if name == 'nt':
+        logging.info('Restart on Windows')
+        exec(open('/home/pi/bot/bot_main.py').read())
+    else:
+        logging.info(f'Restart on {name}')
+        python = sys.executable
+        execl(python, python, *sys.argv)
