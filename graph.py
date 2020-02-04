4cc4191b93bc5e60272ecd41bf42319aef396080
sashasul2003@gmail.com <sashasul2003@gmail.com>
2020-02-04 13:52:13 +0300

    Make new file for graph making

--- graph.py
+++ graph.py
@@ -1,2 +1,15 @@
+import asyncio
+import aiohttp
 import matplotlib.pyplot as plt
+from bs4 import BeautifulSoup
+from os import path, name
+
+if name == 'nt':
+    path = path.dirname(__file__)+'/'
+else:
+    path = '/home/pi/bot/'
+
+IP_ADD = 'http://'+'192.168.1.52'+'/values'
+
+async def get_info(IP_ADD, session):
 
