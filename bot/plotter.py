4575815d28c28c4200c6ce59a4eb6cd45bbf5ec8
aleksul <sashasul2003@gmail.com>
2021-04-13 00:44:35 +0300

    Finish rewriting plotter for better performance

--- bot/plotter.py
+++ bot/plotter.py
@@ -5,91 +5,27 @@ from matplotlib.dates import DateFormatter, MinuteLocator, DayLocator
 from matplotlib.ticker import NullFormatter
 from matplotlib.figure import Figure
 
-from datetime import date, datetime, timedelta
-
 from io import BytesIO
 
-from models import OneMinuteData, PlotData, PlotDayData, PlotMonthData
+from models import PlotData, PlotDayData, PlotMonthData
+
+trans_values = {
+    "pm25": "Частицы PM2.5",
+    "pm10": "Частицы PM10",
+    "temperature": "Температура",
+    "pressure": "Давление",
+    "humidity": "Влажность",
+}
+trans_values_and_units = {
+    "pm25": "Частицы PM2.5, мкгр/м³",
+    "pm10": "Частицы PM10, мкгр/м³",
+    "temperature": "Температура, °C",
+    "pressure": "Давление, мм/рт.ст.",
+    "humidity": "Влажность, %",
+}
 
 
-class Plotter:
-    # TODO rewrite plot minutes
-
-    @staticmethod
-    def valueToStr(value: str):
-        value_dict = {
-            "pm25": "Частицы PM2.5",
-            "pm10": "Частицы PM10",
-            "temperature": "Температура",
-            "pressure": "Давление",
-            "humidity": "Влажность",
-        }
-        result = value_dict.get(value)
-        if result is None:
-            logging.error(f"Can not convert value ({value}) to string!")
-        return result
-
-    @staticmethod
-    def timeToStr(time: str):
-        time_dict = {
-            "015": "15 минут",
-            "030": "полчаса",
-            "060": "1 час",
-            "180": "3 часа",
-            "day": "день",
-            "mon": "месяц",
-        }
-        result = time_dict.get(time)
-        if result is None:
-            logging.error(f"Wrong time: {time}")
-        return result
-
-    @staticmethod
-    def valueToStrWithUnits(value: str) -> str:
-        """Translates value
-
-        Args:
-            value (str): value to translate
-
-        Raises:
-            Exception: value is wrong
-
-        Returns:
-            str: translated value
-        """
-        value_dict = {
-            "pm25": "Частицы PM2.5, мкгр/м³",
-            "pm10": "Частицы PM10, мкгр/м³",
-            "temperature": "Температура, °C",
-            "pressure": "Давление, мм/рт.ст.",
-            "humidity": "Влажность, %",
-        }
-        result = value_dict.get(value)
-        if result is None:
-            logging.error(f"Can not convert value ({value}) to string with units!")
-            raise Exception(f"Wrong value ({value})")
-        return result
-
-    @staticmethod
-    def isValueCorrect(value: str) -> bool:
-        """Checks if value is correct.
-
-        Args:
-            value (str): value to check
-
-        Raises:
-            Exception: value is wrong
-
-        Returns:
-            bool: True, if value is correct
-        """
-        if value not in OneMinuteData.__vars__():
-            logging.error(f"Got wrong value ({value})")
-            raise Exception("Wrong value")
-        return True
-
-
-class MinutesPlotter(Plotter):
+class MinutesPlotter:
 
     def __init__(self):
         self.fig = Figure(figsize=(6, 5), dpi=100)
@@ -100,32 +36,42 @@ class MinutesPlotter(Plotter):
         self.fig.autofmt_xdate()  # rotates xaxis dates tickables for 30*
         # set major formatter to '23:59'
         self.ax.xaxis.set_major_formatter(DateFormatter("%H:%M"))
-        ticks_minute = list(range(0, 60, 5))
-        self.ax.xaxis.set_major_locator(MinuteLocator(byminute=ticks_minute))
+        self.ax.xaxis.set_major_locator(MinuteLocator(byminute=list(range(0, 60, 5))))
         self.ax.xaxis.set_minor_formatter(NullFormatter())
-        every_minute = list(range(0, 60))
-        self.ax.xaxis.set_minor_locator(  # TODO test with 'every minute'
-            MinuteLocator(byminute=[i for i in every_minute if i not in ticks_minute]))
+        self.ax.xaxis.set_minor_locator(MinuteLocator(byminute=list(range(0, 60))))
         # plot
         self.line, = self.ax.plot([], [], marker=".")
 
-    def plot_minutes(self, data: PlotData, value: str) -> bytes:  # TODO add data description
-        """Plots a graph for N minutes
+    def plot_minutes(self, data: PlotData) -> bytes:
+        """Plots a graph for N (3 < N < 330) minutes
 
         Args:
-            data (PlotData): [description]
-            value (str): Name of the plotting info
+            data (PlotData): pydantic model - contains values, time for them and valueName
 
         Returns:
             bytes: Graph photo
         """
         # set new label
-        self.ax.set_ylabel(self.valueToStrWithUnits(value))
+        self.ax.set_ylabel(self.valueToStr(data.valueName, with_units=True))
+        # compress values
         if len(data.values) > 100:
-            return self._plot_lots_of_minutes(data, value)
+            i = data.values
+            t = data.time
+            if len(data.values) < 180:  # compress level 1
+                data.values = [(i[n - 2] + i[n - 1] + i[n]) / 3 for n in range(3, len(i), 3)]
+                data.time = [t[n] for n in range(2, len(t), 3)]
+                self._change_locator_mode(2)
+            elif len(data.values) < 330:  # compress level 2
+                data.values = [(i[n - 4] + i[n - 3] + i[n - 2] + i[n - 1] + i[n]) / 5
+                               for n in range(5, len(i), 5)]
+                data.time = [t[n] for n in range(3, len(t), 5)]
+                self._change_locator_mode(3)
+            else:
+                raise ValueError("Too much values")
+        else:
+            self._change_locator_mode(1)
         # set new data
         self.line.set_data(data.time, data.values)
-
         # redraw
         self.ax.relim()
         self.ax.autoscale_view()
@@ -140,31 +86,25 @@ class MinutesPlotter(Plotter):
         plt.close()
         return buffer
 
-    def _plots_lots_of_minutes(self, data: PlotData, value: str) -> bytes:
-        '''
-        data_temp = convertedData
-            minutes_temp = _time
-            convertedData = []
-            _time = []
-            for i in range(2, len(data_temp), 3):
-                # counts averange of three nearest points
-                convertedData.append(
-                    round((data_temp[i - 2] + data_temp[i - 1] + data_temp[i]) / 3, 2))
-                _time.append(minutes_temp[i - 1])
-        '''
-        '''
-        elif labels_count <= 180:
-            ticks_minute = list(range(0, 60, 10))
-            ax.xaxis.set_major_locator(MinuteLocator(byminute=ticks_minute))
-            ax.xaxis.set_minor_formatter(NullFormatter())
-            every_minute = list(range(0, 60, 2))
-            ax.xaxis.set_minor_locator(
-                MinuteLocator(byminute=[i for i in every_minute if i not in ticks_minute]))
-        '''
-        pass
+    def _change_locator_mode(self, mode: int) -> None:
+        """Changes figure's locator
+
+        Args:
+            mode (int): from 1 to 3. Locator selecter
+        """
+        assert mode in [1, 2, 3], 'Select mode from 1-3'
+        if mode == 1:
+            self.ax.xaxis.set_major_locator(MinuteLocator(byminute=list(range(0, 60, 5))))
+            self.ax.xaxis.set_minor_locator(MinuteLocator(byminute=list(range(0, 60))))
+        elif mode == 2:
+            self.ax.xaxis.set_major_locator(MinuteLocator(byminute=list(range(0, 60, 10))))
+            self.ax.xaxis.set_minor_locator(MinuteLocator(byminute=list(range(0, 60, 2))))
+        elif mode == 3:
+            self.ax.xaxis.set_major_locator(MinuteLocator(byminute=list(range(0, 60, 15))))
+            self.ax.xaxis.set_minor_locator(MinuteLocator(byminute=list(range(0, 60, 3))))
 
 
-class DayPlotter(Plotter):
+class DayPlotter:
 
     def __init__(self):
         self.fig = Figure(figsize=(6, 5), dpi=100)
@@ -194,23 +134,22 @@ class DayPlotter(Plotter):
             borderaxespad=0.0,
         )
 
-    def plot_day(self, data: PlotDayData, value: str) -> bytes:  # TODO add data description
+    def plot_day(self, data: PlotDayData) -> bytes:
         """Plots graph of a day with min and max for four time periods
 
         Args:
-            data (PlotDayData): [description]
-            value (str): Name of the plotting info
+            data (PlotDayData): pydantic model - contains all day parts, value name and date
 
         Returns:
             bytes: graph photo
         """
         # set new label
-        self.ax.set_ylabel(self.valueToStrWithUnits(value))
+        self.ax.set_ylabel(self.valueToStr(data.valueName, with_units=True))
         # find max and min per day
         maximum = max(
-            [data.morning.maximum, data.noon.maximum, data.evening.maximum, data.night.maximum])
+            (data.morning.maximum, data.noon.maximum, data.evening.maximum, data.night.maximum))
         minimum = min(
-            [data.morning.minimum, data.noon.minimum, data.evening.minimum, data.night.minimum])
+            (data.morning.minimum, data.noon.minimum, data.evening.minimum, data.night.minimum))
         # set new data
         self.min_bar.patches[0].set_height(data.morning.minimum)
         self.min_bar.patches[1].set_height(data.noon.minimum)
@@ -221,11 +160,11 @@ class DayPlotter(Plotter):
         self.max_bar.patches[2].set_height(data.evening.maximum)
         self.max_bar.patches[3].set_height(data.night.maximum)
         # set limits
-        if value == "pm25" or value == "pm10":
+        if data.valueName == "pm25" or data.valueName == "pm10":
             self.ax.set_ylim(bottom=0, top=maximum + 1)
-        elif value == "temperature":
+        elif data.valueName == "temperature":
             self.ax.set_ylim(bottom=minimum - 1, top=maximum + 1)
-        elif value == "pressure" or value == "humidity":
+        elif data.valueName == "pressure" or data.valueName == "humidity":
             self.ax.set_ylim(bottom=minimum - 3, top=maximum + 3)
         # redraw
         self.ax.relim()
@@ -242,7 +181,7 @@ class DayPlotter(Plotter):
         return buffer
 
 
-class MonthPlotter(Plotter):
+class MonthPlotter:
 
     def __init__(self):
         self.fig = Figure(figsize=(6, 5), dpi=100)
@@ -266,18 +205,18 @@ class MonthPlotter(Plotter):
             borderaxespad=0.0,
         )
 
-    def plot_month(self, data: PlotMonthData, value: str) -> bytes:  # TODO add data description
+    def plot_month(self, data: PlotMonthData) -> bytes:
         """Plots month graph with min and max values of the day
 
         Args:
-            data (PlotMonthData): [description]
-            value (str): Name of the plotting info
+            data (PlotMonthData): pydantic model - contains all dates and max/min for them,
+            as well as value name
 
         Returns:
             bytes: graph photo
         """
         # set new label
-        self.ax.set_ylabel(self.valueToStrWithUnits(value))
+        self.ax.set_ylabel(self.valueToStr(data.valueName, with_units=True))
         # set new data
         self.min_line.set_data(data.dates, data.minimum)
         self.max_line.set_data(data.dates, data.maximum)
@@ -293,3 +232,30 @@ class MonthPlotter(Plotter):
         buffer = notFile.getvalue()
         notFile.close()
         return buffer
+
+
+class Plotter(MinutesPlotter, DayPlotter, MonthPlotter):
+
+    @staticmethod
+    def valueToStr(value: str, with_units=False) -> str:
+        """Translates value
+
+        Args:
+            value (str): value to convert
+            with_units (bool, optional): to use measurment units or not. Defaults to False.
+
+        Raises:
+            ValueError: value is wrong
+
+        Returns:
+            str: translated value
+        """
+        global trans_values, trans_values_and_units
+        if not with_units:
+            result = trans_values.get(value)
+        else:
+            result = trans_values_and_units.get(value)
+        if result is None:
+            logging.error(f"Can not convert value ({value}) to string!")
+            raise ValueError(f"Wrong value ({value})")
+        return result
